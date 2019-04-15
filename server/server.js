dce57d884f0a1c84c4eaf7f928d342f4ce6a9afa
Thomas Berhe Ghebreyohannes <Woolu@Thomass-MacBook-Pro.local>
2019-04-15 09:52:28 -0700

    check-in

--- server/server.js
+++ server/server.js
@@ -22,6 +22,15 @@ app.use(session({
     saveUninitialized: false
 }))
 
+app.post('/api/auth/register', authCtrl.register)
+app.post('/api/auth/login', authCtrl.login)
+app.put('/api/goals', authCtrl.goals)
+app.put('/api/check-in', authCtrl.checkIn)
+app.get('/api/home', authCtrl.home)
+app.get('/api/profile', authCtrl.profile)
+app.get('/api/reports', authCtrl.reports)
+app.get('/api/reports/food', authCtrl.food)
+app.get('/api/reports/exercise', authCtrl.exercise)
 app.get('/api/logout', (req, res) => {
     req.session.destroy()
     res.redirect('http://localhost:3000/')
