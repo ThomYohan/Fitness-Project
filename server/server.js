03e8579b9de55f1b836eef98c837673b924bdb27
Thomas Berhe Ghebreyohannes <Woolu@Thomass-MacBook-Pro.local>
2019-04-15 09:16:33 -0700

    just starting out

---
+++ server/server.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+const express = require('express')
+const massive = require('massive')
+const session = require('express-session')
+const authCtrl = require('./authCtrl')
+
+const { SERVER_PORT, CONNECTION_STRING } = process.env
+
+const app = express()
+
+massive(CONNECTION_STRING).then(db => {
+    app.set('db', db)
+    app.listen(SERVER_PORT, () => {
+        console.log(`Listening on port: ${SERVER_PORT}`)
+    })
+})
+
+app.use(express.json())
+app.use(session({
+    secret: SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+
+app.get('/api/logout', (req, res) => {
+    req.session.destroy()
+    res.redirect('http://localhost:3000/')
+})
